a94c571cb7bc331cdfc65828fe7311d7e0b247af
Robzera <souza.robson159@gmail.com>
2025-01-03 19:24:46 -0300

    Projeto Boilerplate

---
+++ Boilerplate.Api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Boilerplate.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Boilerplate.Api.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<User, IdentityRole<long>, long,IdentityUserClaim<long>,IdentityUserRole<long>,IdentityUserLogin<long>,IdentityRoleClaim<long>,IdentityUserToken<long>>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
